3c8958a56d0a470d6f956589a32b0f267a3618f3
Elucia <abbyc9113@gmail.com>
2026-03-15 04:09:12 -0700

    fixed small text position bug

--- src/scenes/Postcard.js
+++ src/scenes/Postcard.js
@@ -45,7 +45,7 @@ class Postcard extends Phaser.Scene {
 
         this.text1 = this.add.text(200, 150, "To: Drake", menuConfig).setOrigin(0.5).setDepth(1).setAlpha(0)
         this.text2 = this.add.text(840, 500, "The time we've spent together\nhere means everything to me.\nI hope you enjoyed this \nplayable postcard.", menuConfig).setOrigin(0.5).setDepth(1).setAlpha(0)
-        this.text3 = this.add.text(790, 700, "Click to show credits, press R to restart.", menuConfig).setOrigin(0.5).setDepth(1).setAlpha(0)
+        this.text3 = this.add.text(780, 685, "Click to show credits, press R to restart.", menuConfig).setOrigin(0.5).setDepth(1).setAlpha(0)
         this.text4 = this.add.text(600, 70, "Credits:", creditsConfig).setOrigin(0.5).setDepth(2).setAlpha(0)
         this.text5 = this.add.text(615, 500, "Art: all made by me\nSound: Paper Rip 1 by Geoff-Bremner-Audio \n https://freesound.org/s/792939/ \nLicense: Creative Commons 0\nVideo Game Music... by Seth_Makes_Sounds\nhttps://freesound.org/s/683835/ \nLicense: Creative Commons 0", creditsConfig).setOrigin(0.5).setDepth(2).setAlpha(0)
 
